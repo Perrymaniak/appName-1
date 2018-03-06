465eee9f86150fd127ffbe2873996f5719e0bcf3
wrzonki <wrzonki@gmail.com>
2018-03-06 16:46:09 +0100

    a czego tu nie ma xD

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
 $( document ).ready(function(){
   $('.modal').modal()
   $(".button-collapse").sideNav({edge: 'right'})
+  $('.articles').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    arrows: true,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 });

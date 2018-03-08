6059ecb5f3885bec1754fe2ae607823ab2349b7b
wrzonki <wrzonki@gmail.com>
2018-03-08 21:36:13 +0100

    Dodano wsparcie dla chroma

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $( document ).ready(function(){
 $("#Rejestracja-password").on("focusout", function (e) {
     if ($(this).val() != $("#Rejestracja-password-ponownie").val()) {
         $("#Rejestracja-password-ponownie").removeClass("valid").addClass("invalid");
+        $("#rejestracja-button").addClass("disabled");
     } else {
         $("#Rejestracja-password-ponownie").removeClass("invalid").addClass("valid");
         $("#rejestracja-button").removeClass("disabled");
@@ -15,6 +16,7 @@ $("#Rejestracja-password").on("focusout", function (e) {
 $("#Rejestracja-password-ponownie").on("keyup", function (e) {
     if ($("#Rejestracja-password").val() != $(this).val()) {
         $(this).removeClass("valid").addClass("invalid");
+        $("#rejestracja-button").addClass("disabled");
     } else {
         $(this).removeClass("invalid").addClass("valid");
         $("#rejestracja-button").removeClass("disabled");

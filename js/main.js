268db4aa0c10328f1276a932ce5cea8343d162e9
wrzonki <wrzonki@gmail.com>
2018-03-08 20:48:32 +0100

    Dodano validacja hasła (ale nawet jak hasła różne to można wysłać :/

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ $( document ).ready(function(){
   $('.modal').modal()
   $(".button-collapse").sideNav({edge: 'right'})
 });
+//Password Validation
+$("#Rejestracja-password").on("focusout", function (e) {
+    if ($(this).val() != $("#Rejestracja-password-ponownie").val()) {
+        $("#Rejestracja-password-ponownie").removeClass("valid").addClass("invalid");
+    } else {
+        $("#Rejestracja-password-ponownie").removeClass("invalid").addClass("valid");
+    }
+});
+
+$("#Rejestracja-password-ponownie").on("keyup", function (e) {
+    if ($("#Rejestracja-password").val() != $(this).val()) {
+        $(this).removeClass("valid").addClass("invalid");
+    } else {
+        $(this).removeClass("invalid").addClass("valid");
+    }
+});
